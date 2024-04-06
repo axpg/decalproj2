becbf083d37c8ba8673b11ae089d1aec8440c1b6
daniel <dgarridoalex@gmail.com>
2024-04-06 15:59:16 -0700

    img

---
+++ my-react-app/src/Components/MembersData.js
@@ -0,0 +1,19 @@
+// membersData.js
+
+const membersData = [
+    {
+      image: 'member1.jpg',
+      name: 'Eddie Cekici',
+      role: 'Co-Business Manager',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      image: require('./img/Ellington.jpeg').default,
+      name: 'Ellington Chen',
+      role: 'Co-Music Manager',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+  ];
+  
+  export default membersData;
+  
