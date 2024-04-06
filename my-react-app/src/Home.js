48aa5486be36a896b3a932fa5728d90a488781a9
evanwoo8 <evanwoo@berkeley.edu>
2024-04-06 15:51:38 -0700

    homejs update

--- my-react-app/src/Home.js
+++ my-react-app/src/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Home.css'; // Import CSS file for styling
 
 function Home() {
   return (
-    <header>
-      <h1>Welcome to My React Homepage!</h1>
-    </header>
+    <div className="container">
+      <header>
+        <img src="noteworthy.png" alt="Noteworthy A Cappella Logo" className="logo" />
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1 className="group-name">Noteworthy A Cappella</h1>
+        {/* Other content */}
+      </main>
+      <footer>
+        {/* Footer content */}
+      </footer>
+    </div>
   );
 }
 
