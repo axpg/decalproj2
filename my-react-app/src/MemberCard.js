c411e84477bdeecb6950684ba2617521b7189381
daniel <dgarridoalex@gmail.com>
2024-04-06 16:32:29 -0700

    img

--- my-react-app/src/MemberCard.js
+++ my-react-app/src/MemberCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 function MemberCard({ member }) {
   return (
     <div className="member-card">
-      <img src={member.image} alt={member.name} />
+      <img src={require(member.image).default} alt={member.name} />
       <h2>{member.name}</h2>
       <p><strong>Role:</strong> {member.role}</p>
       <p>{member.bio}</p>
