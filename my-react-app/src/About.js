6822b2e705e8d445e0eb3276075fd3f354fcacd5
evanwoo8 <evanwoo@berkeley.edu>
2024-04-06 18:45:11 -0700

    sdfsd

--- my-react-app/src/About.js
+++ my-react-app/src/About.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 function About() {
   return (
-    <header>
-      <h1>About us</h1>
-    </header>
+    <div>
+      <header>
+        <h1>Noteworthy</h1>
+      </header>
+      <div>
+        <h3>In-vest-ed in A Cappella</h3>
+        <p>We’re a lower-voices a cappella group known for our fun performances and fresh takes on popular songs! It always feels sunny when we perform, and not just because of our golden vests!</p>
+        <p>Since our founding in 2000, we’ve gone million-view-viral on YouTube multiple times, released an album entirely composed of original songs, and performed at the 2022 UC Berkeley commencement.</p>
+      </div>
+    </div>
   );
-}
+  }
 
 export default About;
