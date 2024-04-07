c3af45208af56cf782e6601bbcd3bf5b36138d7b
evanwoo8 <evanwoo@berkeley.edu>
2024-04-06 17:39:56 -0700

    sdfs

--- my-react-app/src/MembersData.js
+++ my-react-app/src/MembersData.js
@@ -1,6 +1,21 @@
 // membersData.js
 import EddieImage from './Images/Eddie.jpeg'
 import EllingtonImage from './Images/Ellington.jpeg'
+import AthenaImage from './Images/athena.jpeg'
+import bhaskerimage from './Images/bhasker.jpeg'
+import tomimage from './Images/tom.jpeg'
+import andrewimage from './Images/andrew.jpeg'
+import stephanimage from './Images/stephan.jpeg'
+import jessicaimage from './Images/jessica.jpeg'
+import jerryimage from './Images/jerry.jpeg'
+import aleximage from './Images/alex.jpeg'
+import kianimage from './Images/kian.jpeg'
+import caitlynimage from './Images/caitlyn.jpeg'
+import mayaimage from './Images/maya.jpeg'
+import amandaimage from './Images/amanda.jpeg'
+import justinimage from './Images/justin.jpeg'
+
+
 
 const membersData = [
     {
@@ -16,79 +31,79 @@ const membersData = [
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EddieImage,
+      image: AthenaImage,
       name: 'Athena Liu',
       role: 'Co-Music Manager',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EllingtonImage,
+      image: bhaskerimage,
       name: 'Bhaskar Roberts',
       role: 'Co-Business Manager',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EddieImage,
+      image: tomimage,
       name: 'Tom Guan',
       role: 'Retired',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EllingtonImage,
+      image: andrewimage,
       name: 'Andrew Tran',
       role: 'Co-Music Manager',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EllingtonImage,
+      image: stephanimage,
       name: 'Stephanus Yang',
       role: 'Co-Music Manager',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EddieImage,
+      image: jessicaimage,
       name: 'Jessica Yu',
       role: 'Co-Music Manager',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EllingtonImage,
+      image: jerryimage,
       name: 'Jerry Hou',
       role: 'Co-Business Manager',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EddieImage,
+      image: aleximage,
       name: 'Alex Pease',
       role: 'Retired',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EllingtonImage,
+      image: kianimage,
       name: 'Kian Chou',
       role: 'Co-Music Manager',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EllingtonImage,
+      image: caitlynimage,
       name: 'Caitlyn Liao',
       role: 'Co-Music Manager',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EddieImage,
+      image: mayaimage,
       name: 'Maya Rotenberg',
       role: 'Co-Music Manager',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EllingtonImage,
+      image: amandaimage,
       name: 'Amanda Sakimura',
       role: 'Co-Business Manager',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      image: EddieImage,
+      image: justinimage,
       name: 'Justin Wong',
       role: 'Retired',
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
